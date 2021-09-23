8f2b5ea641ba9fa48b3442909a50814d9c3a14b6
Piyush <44377484+piyush19198@users.noreply.github.com>
2021-09-23 22:53:53 +0530

    Add files via upload

---
+++ Assignment7.java
@@ -0,0 +1,28 @@
+package assignments1;
+/*
+A shop will give discount of 10% if the cost of purchased quantity is
+more than 1000.Ask user for quantity and suppose, one unit will cost
+100. Calculate and print total cost for user.
+*/
+import java.util.Scanner;
+
+public class Assignment7 {
+
+	public static void main(String[] args) {
+		int q,p=100;
+		double fp;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Quantity :");
+		q = sc.nextInt();
+		
+		if(q>1000) {
+			fp=(q*p)*0.9;
+			System.out.printf("Discount Applied. Final price is : %.2f",fp);
+		}
+		
+		else
+			System.out.println("Discount not applied. Final price is : " +(q*p));
+			
+	}
+
+}
